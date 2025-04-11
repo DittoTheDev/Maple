e023d5f81211b01224ed18935473348d11231cb4
ItsCodey1095 <aaroh403@gmail.com>
2025-04-11 20:28:26 +0530

    Update compiler.h

--- windows/compiler/compiler.h
+++ windows/compiler/compiler.h
@@ -1,7 +1,8 @@
 #ifndef COMPILER_H
 #define COMPILER_H
 
-// Compile .maple source code into .mbc bytecode (stub function)
+// Compile .maple source code into .mbc bytecode.
+// Returns 0 on success and nonzero on failure.
 int maple_compile(const char* source_filename, const char* output_filename);
 
 #endif // COMPILER_H
