794ecb06350815366b53030ebe4362e3eeca4120
ItsCodey1095 <aaroh403@gmail.com>
2025-04-11 20:25:06 +0530

    Create compiler.h

---
+++ windows/compiler/compiler.h
@@ -0,0 +1,7 @@
+#ifndef COMPILER_H
+#define COMPILER_H
+
+// Compile .maple source code into .mbc bytecode (stub function)
+int maple_compile(const char* source_filename, const char* output_filename);
+
+#endif // COMPILER_H
