6de903bc8a1b13b7cc3d87a947822b58ccb4fe86
ItsCodey1095 <aaroh403@gmail.com>
2025-04-11 19:19:03 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
+#include <string.h>
 #include "engine/engine.h"
+
+#define MAPLE_VERSION "1.1 Beta"
 
 int main(int argc, char **argv) {
     if (argc < 2) {
-        printf("Usage: mvm <filename.mbc>\n");
+        printf("Usage: mvm <filename.mbc> | --version\n");
         return 1;
     }
 
+    // If the first argument is a version flag, print version info.
+    if ((strcmp(argv[1], "--version") == 0) || (strcmp(argv[1], "-v") == 0)) {
+        printf("Maple version %s\n", MAPLE_VERSION);
+        return 0;
+    }
+
     MapleEngine *engine = maple_create();
+    if (engine == NULL) {
+        fprintf(stderr, "Error: Failed to initialize the Maple engine.\n");
+        return 1;
+    }
+
     maple_run(engine, argv[1]);
     maple_destroy(engine);
+
     return 0;
 }
