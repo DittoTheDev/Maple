f6b34b3364bac3b4cba0e4c6f2364e5c1eef77c9
ItsCodey1095 <aaroh403@gmail.com>
2025-04-11 20:27:29 +0530

    Delete engine directory

--- engine/compiler.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "compiler/compiler.h"
-
-int maple_compile(const char* source_filename, const char* output_filename) {
-    // This stub simulates compilation.
-    printf("Compiling %s to %s ... (stub implementation)\n", source_filename, output_filename);
-    // In a full implementation, source code parsing, bytecode generation, and saving would occur.
-    return 0;
-}
--- engine/compiler.h
+++
@@ -1,7 +0,0 @@
-#ifndef COMPILER_H
-#define COMPILER_H
-
-// Compile .maple source code into .mbc bytecode.
-int maple_compile(const char* source_filename, const char* output_filename);
-
-#endif // COMPILER_H
