64011fedb99aa45c68043759909c418f56b587ee
ItsCodey1095 <aaroh403@gmail.com>
2025-04-12 07:54:42 +0000

    Initial import: Maple compiler, VM, CMake files, plugins template

--- windows/compiler/compiler.h
+++
@@ -1,8 +0,0 @@
-#ifndef COMPILER_H
-#define COMPILER_H
-
-// Compile .maple source code into .mbc bytecode.
-// Returns 0 on success and nonzero on failure.
-int maple_compile(const char* source_filename, const char* output_filename);
-
-#endif // COMPILER_H
